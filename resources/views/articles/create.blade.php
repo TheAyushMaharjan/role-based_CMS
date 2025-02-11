2b831fdb3656d687db8068a5d5978a02fb48760d
Ayush Maharjan <Ayush.maharjan399@gmail.com>
2025-02-11 16:55:08 +0545

    ui_create_article

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -14,16 +14,45 @@
                         @csrf
                         <div>
                             <div class="container flex justify-between items-center">
-                                <label for="" class="text-xl font-medium">Name</label>
+                                <label for="" class="text-xl font-medium">Create new article</label>
+
                                 <div class="create  ">
                                     <a class=" px-4 py-2 flex items-center" href="{{route('permissions.index')}}"><i
                                             class="fa-solid fa-angle-left text-2xl p-3"></i> Back</a>
                                 </div>
                             </div>
+                            {{-- Title --}}
+                            <div class="container flex justify-between items-center">
+                                <label for="" class="text-xl font-medium m-2">Title</label> 
+                            </div>
                             <div class="mb-3">
-                                <input value="{{old('name')}}" name="name" placeholder="Enter permission name"
+                                <input value="{{old('title')}}" name="title" placeholder="Enter the title"
                                     type="text" class="border-gray-300 shadow-sm w-1/2 rounded-lg">
-                                @error('name')
+                                @error('title')
+                                <p class="text-red-400">{{$message}}</p>
+                                @enderror
+                            </div>
+
+                             {{-- Text --}}
+                             <div class="container flex justify-between items-center">
+                                <label for="" class="text-xl font-medium m-2">Text</label> 
+                            </div>
+                            <div class="mb-3">
+                                <input value="{{old('text')}}" name="text" placeholder="Enter the text"
+                                    type="text" class="border-gray-300 shadow-sm w-1/2 rounded-lg">
+                                @error('text')
+                                <p class="text-red-400">{{$message}}</p>
+                                @enderror
+                            </div>
+
+                             {{-- Author --}}
+                             <div class="container flex justify-between items-center">
+                                <label for="" class="text-xl font-medium m-2">Author</label> 
+                            </div>
+                            <div class="mb-3">
+                                <input value="{{old('author')}}" name="author" placeholder="Enter the author"
+                                    type="text" class="border-gray-300 shadow-sm w-1/2 rounded-lg">
+                                @error('author')
                                 <p class="text-red-400">{{$message}}</p>
                                 @enderror
                             </div>
